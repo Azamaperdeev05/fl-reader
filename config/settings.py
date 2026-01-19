96a532446b91756b937bd85f9fcd27dc075fa89d
Azamat Perdeev <127682978+Azamaperdeev05@users.noreply.github.com>
2026-01-19 19:07:18 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -129,15 +129,6 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
